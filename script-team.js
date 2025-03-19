f49f3d0db98504bd196c6d5716cc653a5553011f
Sannidhya Sharma <80062054+SleeperOfSaturn@users.noreply.github.com>
2025-03-19 07:47:55 +0530

    Uploading alt_team page

---
+++ script-team.js
@@ -0,0 +1,11 @@
+/*
+My take on the Cool Shuffling Effect in Pure CSS as created by Toni Lijic @tonilijic
+https://www.toni.li/about
+
+Chris Coyer @chriscoyier  and Jason Lengstorf  @jlengstorf demonstrated how they would approach this on a live stream (which I missed). 
+Before watching their stream or looking at any source code I wanted to have a go myself.
+
+This is a pure CSS version. Not overly practical but still fun to do.
+Still a but buggy regarding the ordering when going backwards and dubious as regards accesibility
+
+*/
